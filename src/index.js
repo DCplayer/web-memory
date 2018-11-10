df990fe0bafc592747e1aea13a48c4961bf31f13
CoffeeBrewer <cas15151@uvg.edu.gt>
2018-11-10 05:30:16 -0600

    randomizador de cartas funcional , comenzando la funcion para el click

--- src/index.js
+++ src/index.js
@@ -14,18 +14,52 @@ class Memoria extends React.Component{
 			selected: false, 
 			card_selected: null, 
 			table: [],
-			spaces: 10, 
-			order: Array(10).fill(null), 
+			spaces: 5, 
+			indexes: Array(10).fill(null),
+			images: Array(10).fill(null), 
 			pairs: 0, 
 			turns: 0
 		}
 
-		create_memory(); 
+		this.create_memory(); 
 	}
 
 	create_memory(){
+		let cards = [c1, c1, c2, c2, c3, c3, c4, c4, c5, c5]; 
+		let remaining = [...Array(this.state.spaces * 2).keys()]; 
 
+		for (let i = 0; i< this.state.spaces; i++){
+			let first_index = i * 2; 
+			let second_index = i * 2 + 1; 
+
+			let fst_card = Math.floor(Math.random()*remaining.length);
+			this.state.indexes[first_index] = remaining[fst_card];
+			this.state.images[first_index] = cards[remaining[fst_card]]; 
+			remaining.splice(fst_card, 1);
+
+			let snd_card = Math.floor(Math.random()*remaining.length);
+			this.state.indexes[second_index] = remaining[snd_card];
+			this.state.images[second_index] = cards[remaining[snd_card]]; 
+			remaining.splice(snd_card, 1);
+		}
+
+		console.log('Prueba, shuffle de las imagenes');
+		console.log(this.state.images);
+		console.log(this.state.indexes); 
 	}
 
 
+
+	render(){
+		return(
+			<div>Hello World!</div>
+
+			)
+	}
 }
+
+
+ReactDom.render(
+	<Memoria />,
+	document.getElementById('root')
+)
